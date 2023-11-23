c922157b6a013eb4e8756bee1239138b849a2948
Sebastien Viguier <viguier.sebastien@orange.fr>
2023-11-23 22:01:40 +0100

    github pages deploy wf

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,28 @@
-import "./style.css";
+import './style.css';
 
-const videoFrame = document.querySelector("video")!;
-const infoDiv = document.querySelector(".info-wrapper")!;
+const videoFrame = document.querySelector('video')!;
+const infoDiv = document.querySelector('.info-wrapper')!;
 
 navigator.mediaDevices
-    .getUserMedia({
-        video: { width: window.screen.width, height: window.screen.height },
-    })
-    .then((stream) => {
-        videoFrame.width = window.screen.availWidth;
-        videoFrame.height = window.screen.availHeight;
-        videoFrame.srcObject = stream;
+  .getUserMedia({
+    video: { width: window.screen.width, height: window.screen.height },
+  })
+  .then(stream => {
+    videoFrame.width = window.screen.availWidth;
+    videoFrame.height = window.screen.availHeight;
+    videoFrame.srcObject = stream;
 
-        setInterval(() => {
-            videoFrame.setAttribute("style", `transform: translate(-${window.screenX}px, -${window.screenY}px)`);
-        }, 10);
+    setInterval(() => {
+      videoFrame.setAttribute(
+        'style',
+        `transform: translate(-${window.screenX}px, -${window.screenY}px)`
+      );
+    }, 10);
 
-        videoFrame.play();
+    videoFrame.play();
 
-        infoDiv.classList.add("hide");
-    })
-    .catch((err) => {
-        console.error(err);
-    });
-
+    infoDiv.classList.add('hide');
+  })
+  .catch(err => {
+    console.error(err);
+  });
